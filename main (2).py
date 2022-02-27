b0854b61fbb8a072f7468f42e74ac8ae38f2c3db
Coding284338 <86387056+Coding284338@users.noreply.github.com>
2022-02-27 13:09:42 -0600

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,12 @@
+a = int(input("type here first for subtraction"));
+b = input("type here second for subtraction");
+print(a-b);
+
+a = int(input("type here first for multiplication"));
+b = input("type here second for multiplication");
+print(a*b);
+
+a = int(input("type here first for division"));
+b = input("type here second for division");
+print(a/b);
+
